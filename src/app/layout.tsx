616449d56a1074faff70287dd8e5e8daa45383c0
Alvarado08 <jorgealvarado0238@gmail.com>
2025-03-20 23:04:31 -0500

    Vercel analytics pckg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
@@ -42,7 +43,10 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <NavBar />
-          <main className="max-w-4xl mx-auto p-4">{children}</main>
+          <main className="max-w-4xl mx-auto p-4">
+            {children}
+            <Analytics />
+          </main>
         </ThemeProvider>
       </body>
     </html>
