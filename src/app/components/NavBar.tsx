9fa414d8b91df89228a43413e4dbd1b7f441c44e
Alvarado08 <jorgealvarado0238@gmail.com>
2025-03-20 22:47:01 -0500

    Fixed types and Link component

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,18 +1,19 @@
 import { ModeToggle } from "./ModeToggle";
 import Image from "next/image";
 import logo from "../../../public/logo.png";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav className="flex justify-between items-center max-w-4xl mx-auto p-4">
-      <a href="/">
+      <Link href="/">
         <Image
           src={logo}
           width={100}
           height={100}
           alt="Iglesia Dios Proveera Logo"
         />
-      </a>
+      </Link>
       <ModeToggle />
     </nav>
   );
--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -2,6 +2,6 @@ export default interface postCard {
   _id: string;
   title: string;
   description: string;
-  slug: any;
-  image: any;
+  slug: string;
+  image: string | null;
 }
